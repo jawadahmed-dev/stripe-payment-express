1c3e395cf13c7c72a3d5291dc4aeef0829d6b33a
jawadahmed.dev <jawadahmed.dev@gmail.com>
2025-06-01 12:24:55 +0500

    Dockerized api project

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -1,8 +1,9 @@
 // src/config/db.ts
 import mongoose from 'mongoose';
+console.log("🔍 MONGODB_URI:", process.env.MONGODB_URI);
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI!);
+  await mongoose.connect(process.env.MONGODB_URI!);
   console.log('✅ MongoDB connected');
 };
 
